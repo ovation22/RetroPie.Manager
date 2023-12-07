552cb6ae2680d966ea2cb209ed179e26d38599cb
ovation22 <john@1north.com>
2023-12-07 09:42:23 -0500

    Show CPU stats for Linux

--- src/RetroPie.Manager.Web/Services/CpuLinuxService.cs
+++ src/RetroPie.Manager.Web/Services/CpuLinuxService.cs
@@ -6,75 +6,81 @@ namespace RetroPie.Manager.Web.Services;
 
 public class CpuLinuxService : ICpuService
 {
-    public Task<CpuStats> GetCpuStats()
+    public async Task<CpuStats> GetCpuStats()
     {
-        var overallCpuUsage = Task.Run(GetCpuUsage);
-        var coreUsages = Task.Run(GetCpuCoreUsage);
+        var output = await RunCommandAsync("cat", "/proc/stat");
+        
+        var lines = output.Split('\n', StringSplitOptions.RemoveEmptyEntries);
 
-        Task.WaitAll(overallCpuUsage, coreUsages);
-
-        return Task.FromResult(new CpuStats
+        var cpuStats = new CpuStats
         {
-            OverallCpuUsage = overallCpuUsage.Result,
-            CoreUsages = coreUsages.Result
-        });
+            OverallCpuUsage = GetOverallCpuUsage(lines),
+            CoreUsages = GetCoreUsages(lines)
+        };
+
+        return cpuStats;
     }
 
-    private static float GetCpuUsage()
+    private static float GetOverallCpuUsage(IEnumerable<string> lines)
     {
-        var output = RunCommand("cat", "/proc/stat");
-        var values = output.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        float overallCpuUsage = 0;
+        var overallCpuLine = lines.FirstOrDefault(l => l.StartsWith("cpu "));
+        
+        if (overallCpuLine == null)
+        {
+            return overallCpuUsage;
+        }
 
-        var user = ulong.Parse(values[1]);
-        var nice = ulong.Parse(values[2]);
-        var system = ulong.Parse(values[3]);
-        var idle = ulong.Parse(values[4]);
+        var overallCpuValues = overallCpuLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (overallCpuValues.Length < 5)
+        {
+            return overallCpuUsage;
+        }
+
+        var user = ulong.Parse(overallCpuValues[1]);
+        var nice = ulong.Parse(overallCpuValues[2]);
+        var system = ulong.Parse(overallCpuValues[3]);
+        var idle = ulong.Parse(overallCpuValues[4]);
 
         var total = user + nice + system + idle;
         var busy = total - idle;
 
-        return (float)busy / total * 100 * 100;
+        overallCpuUsage = (float)busy / total * 100 * 100;
+
+        return overallCpuUsage;
     }
 
-    private static (byte core, float usage)[] GetCpuCoreUsage()
+    private static (byte core, float usage)[] GetCoreUsages(IEnumerable<string> lines)
     {
-        var coreCount = Environment.ProcessorCount;
-        var usageValues = new (byte core, float usage)[coreCount];
+        return lines.Skip(1).Where(l => l.StartsWith("cpu"))
+            .Select(line =>
+            {
+                var coreValues = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-        Parallel.For((long)0, coreCount, core =>
-        {
-            usageValues[core] = ((byte)core, GetCoreUsage(core));
-        });
+                if (coreValues.Length < 5)
+                {
+                    Console.WriteLine($"Error: Unable to retrieve CPU core usage for {line}");
+                    return (core: (byte)0, usage: 0);
+                }
 
-        return usageValues;
+                var coreIndex = byte.Parse(coreValues[0][3..]);
+                var user = ulong.Parse(coreValues[1]);
+                var nice = ulong.Parse(coreValues[2]);
+                var system = ulong.Parse(coreValues[3]);
+                var idle = ulong.Parse(coreValues[4]);
+
+                var total = user + nice + system + idle;
+                var busy = total - idle;
+
+                var usage = (float)busy / total * 100 * 100;
+
+                return (core: coreIndex, usage);
+            })
+            .ToArray();
     }
 
-    private static float GetCoreUsage(long core)
-    {
-        var output = RunCommand("cat", "/proc/stat");
-        var line = output.Split('\n')
-            .FirstOrDefault(l => l.StartsWith($"cpu{core} "));
-
-        if (line == null)
-        {
-            Console.WriteLine($"Error: CPU core {core} not found in /proc/stat");
-            return 0;
-        }
-
-        var values = output.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        ulong user = ulong.Parse(values[1]);
-        ulong nice = ulong.Parse(values[2]);
-        ulong system = ulong.Parse(values[3]);
-        ulong idle = ulong.Parse(values[4]);
-
-        ulong total = user + nice + system + idle;
-        ulong busy = total - idle;
-
-        return (float)busy / total * 100 * 100;
-    }
-
-    private static string RunCommand(string command, params string[] arguments)
+    private static async Task<string> RunCommandAsync(string command, params string[] arguments)
     {
         using var process = new Process();
         process.StartInfo.FileName = command;
@@ -82,10 +88,12 @@ public class CpuLinuxService : ICpuService
         process.StartInfo.RedirectStandardOutput = true;
         process.StartInfo.UseShellExecute = false;
         process.StartInfo.CreateNoWindow = true;
+
         process.Start();
 
-        var result = process.StandardOutput.ReadToEnd();
-        process.WaitForExit();
+        var result = await process.StandardOutput.ReadToEndAsync();
+        await process.WaitForExitAsync();
+
         return result;
     }
 }
