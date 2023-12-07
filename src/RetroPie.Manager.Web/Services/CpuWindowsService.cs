97fded4106f0b65729b0d55aa004ac3f4160b8e1
ovation22 <john@1north.com>
2023-12-07 13:35:27 -0500

    Show CPU stats for Windows

--- src/RetroPie.Manager.Web/Services/CpuWindowsService.cs
+++ src/RetroPie.Manager.Web/Services/CpuWindowsService.cs
@@ -6,46 +6,54 @@ namespace RetroPie.Manager.Web.Services;
 
 public class CpuWindowsService : ICpuService
 {
-    public Task<CpuStats> GetCpuStats()
+    public async Task<CpuStats> GetCpuStats()
     {
-        var overallCpuUsage = Task.Run(GetCpuUsage);
-        var coreUsages = Task.Run(GetCpuCoreUsage);
+        var overallCpuUsage = GetCpuUsage();
+        var coreUsages = GetCoreUsages();
 
-        Task.WaitAll(overallCpuUsage, coreUsages);
+        await Task.WhenAll(overallCpuUsage, coreUsages);
 
-        return Task.FromResult(new CpuStats
+        return new CpuStats
         {
             OverallCpuUsage = overallCpuUsage.Result,
             CoreUsages = coreUsages.Result
-        });
+        };
     }
 
-    private static float GetCpuUsage()
+    private static async Task<float> GetCpuUsage()
     {
         using var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
         cpuCounter.NextValue(); // Call this to get initial value
-        Thread.Sleep(1000);     // Sleep for 1 second
+        await Task.Delay(1000); // Asynchronously sleep for 1 second
         return cpuCounter.NextValue();
     }
 
-    private static (byte core, float usage)[] GetCpuCoreUsage()
+    private static async Task<(byte core, float usage)[]> GetCoreUsages()
     {
         var coreCount = Environment.ProcessorCount;
         var usageValues = new (byte core, float usage)[coreCount];
+        var tasks = new Task<float>[coreCount];
 
-        Parallel.For((long)0, coreCount, core =>
+        for (int i = 0; i < coreCount; i++)
         {
-            usageValues[core] = ((byte)core, GetCoreUsage(core));
-        });
+            tasks[i] = GetCoreUsage(i);
+        }
 
+        await Task.WhenAll(tasks);
+
+        for (int core = 0; core < coreCount; core++)
+        {
+            usageValues[core] = ((byte)core, tasks[core].Result);
+        }
+        
         return usageValues;
     }
 
-    private static float GetCoreUsage(long core)
+    private static async Task<float> GetCoreUsage(long core)
     {
         using var coreCounter = new PerformanceCounter("Processor", "% Processor Time", $"{core}");
         coreCounter.NextValue(); // Call this to get initial value
-        Thread.Sleep(1000);     // Sleep for 1 second
+        await Task.Delay(1000);    // Sleep for 1 second
         return coreCounter.NextValue();
     }
 }
