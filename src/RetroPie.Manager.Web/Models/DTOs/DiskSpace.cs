7e717eb7413e129e9af624d1cbe2d12a27bc83d0
ovation22 <john@1north.com>
2023-12-08 09:48:25 -0500

    Show disk stats on Windows

---
+++ src/RetroPie.Manager.Web/Models/DTOs/DiskSpace.cs
@@ -0,0 +1,9 @@
+﻿namespace RetroPie.Manager.Web.Models.DTOs;
+
+public class DiskSpace
+{
+    public float OverallUsagePercent { get; set; }
+    public float FreeSpace { get; set; }
+    public float AvailableSpacePercent { get; set; }
+    public float TotalSpace { get; set; }
+}
