4f44832d3eed0d74787b599031e7e09fceb0f152
ovation22 <john@1north.com>
2023-12-10 14:48:37 -0500

    Get ROM files

---
+++ src/RetroPie.Manager.Web/Models/DTOs/RomFile.cs
@@ -0,0 +1,10 @@
+﻿namespace RetroPie.Manager.Web.Models.DTOs;
+
+public class RomFile
+{
+    public string FileName { get; set; } = default!;
+    public long FileSize { get; set; }
+    public DateTime CreationTime { get; set; }
+    public DateTime LastAccessTime { get; set; }
+    public DateTime LastWriteTime { get; set; }
+}
